4f3155b9c68cfa57aa40986a29d0c7be411e8fb7
Luke Bollwerk <lukas8@gmail.com>
2023-11-27 13:13:08 -0700

    upload snapshot

--- src/main/java/com/bollwerks/eznav/model/ScreenConfig.kt
+++ src/main/java/com/bollwerks/eznav/model/ScreenConfig.kt
@@ -3,7 +3,6 @@ package com.bollwerks.eznav.model
 import androidx.compose.material3.DrawerState
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavController
 import com.bollwerks.eznav.EzRoute
 
